f35b47830819ed016f060a0c6a3f501d6883ba8c
Arda Baki <ardabaki52@gmail.com>
2026-04-20 00:15:04 +0300

    feat: use create command for create orders table

--- 01-basics/01-use-alter-command-for-product-table.sql
+++ 01-basics/01-use-alter-command-for-product-table.sql
@@ -19,16 +19,37 @@
 
 -------------------------------------------------
 
-	--Topic : Use DDL Commands for Create Table
+	--Topic : Use Create Command for Create Customers Table
 
 
-Create Table Customers
-(
-CustomerId int primary key identity(1,1),
-CustomerName nvarchar(50),
-CustomerSurname nvarchar(50),
-CustomerEmail nvarchar(100),
-CustomerrDistrict nvarchar(50),
-CustomerCity nvarchar(50),
-CustomerCountry nvarchar(50)
-)
+--Create Table Customers
+--(
+--CustomerId int primary key identity(1,1),
+--CustomerName nvarchar(50),
+--CustomerSurname nvarchar(50),
+--CustomerEmail nvarchar(100),
+--CustomerrDistrict nvarchar(50),
+--CustomerCity nvarchar(50),
+--CustomerCountry nvarchar(50)
+--)
+
+
+---------------------------------------------------
+
+	--Topic : Use Create Command for Create Order Table
+
+
+--Create Table Orders
+--(
+--OrderId int primary key identity(1,1),
+--CustomerId int,
+--ProductId int,
+--OrderDate Datetime,
+--Quantity int,
+--LineTotal decimal(18,2),
+--PaymentMethod nvarchar(50),
+--OrderStatus nvarchar(50),
+
+--)
+
+------------------------------------------------------
