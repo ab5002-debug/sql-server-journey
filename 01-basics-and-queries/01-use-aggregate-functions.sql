753225b68a02ed629aa7ba6a36469ca9eb4f8d5d
Arda Baki <ardabaki52@gmail.com>
2026-04-26 21:07:50 +0300

    feat: implement aggregate functions for customer analysis

---
+++ 01-basics-and-queries/01-use-aggregate-functions.sql
@@ -0,0 +1,35 @@
+--Database : ShopBaseDb
+-- Instructor: Murat Yücedağ
+
+--Murat Yücedağ SQL Camp - Lesson 10
+--Topic : Aggregate Functions (Max-Min-Count-Sum-Avg)
+
+--COUNT
+
+Select Count(*) as 'Number of Customers' From Customers
+
+Select Count(*) From Customers Where CustomerCountry = 'Türkiye'
+
+Select Count(*) From Customers Where CustomerCountry = 'Türkiye' and CustomerCity = 'Ankara'
+
+Select Count(*) From Customers Where CustomerCity = 'Adana'
+
+Select Count(*) From Customers Where CustomerCity = 'Ankara'
+
+--SUM
+
+Select Sum(CustomerBalance) From Customers
+
+Select Sum(CustomerBalance) From Customers Where CustomerCity = 'Ankara'
+
+--AVG
+
+Select Avg(CustomerBalance) From Customers Where CustomerCity = 'Ankara'
+
+--MIN-MAX
+
+Select Max(CustomerBalance) From Customers
+
+Select Min(CustomerBalance) From Customers
+
+Select Min(CustomerBalance) From Customers Where CustomerCity = 'İstanbul'
